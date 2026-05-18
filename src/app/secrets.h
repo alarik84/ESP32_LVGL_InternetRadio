77c87b173a2518b2c85473e1d20bfed240af485e
alarik84 <alarik@poczta.onet.pl>
2026-05-18 22:36:04 +0200

    Usuniecie pliku z poufnymi danymi z repozytorium

--- src/app/secrets.h
+++
@@ -1,7 +0,0 @@
-const char *WIFIName = "MnMsy";
-const char *WIFIPasswd = "MnMsyNaWitosa_10-100";
-const char *otaPassword = "Pablo8846!";
-
-const char* timeServer1 = "192.168.1.254";
-const char* timeServer2 = "debserv.mnmsy.net";
-const char* timeServer3 = "tempus1.gum.gov.pl";
